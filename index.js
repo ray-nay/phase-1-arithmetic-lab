551e28724ed886bcef856c65fdf481d55bbbdea1
Renee Mumbi <1723renee@gmail.com>
2022-11-24 13:12:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1=31;
+let num2=2;
 
-// Write your code here
+let multiply;
+multiply = num1*num2;
+multiply=62;
+
+let random=Math.floor(Math.random()*1000+1)
+let num3 =10;
+let num4 = 6;
+let mod = num3 % num4;
+
+let  max=Math.max(3,6,18,20,9);
